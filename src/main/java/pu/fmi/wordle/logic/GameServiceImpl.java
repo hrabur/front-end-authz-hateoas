e22ad0144ed6c07f6ccdaab3526ee9542469e490
Hrabur Trendafilov <hrabur@yahoo.com>
2023-03-12 11:42:00 +0200

    Build Game UI with Thymeleaf and Spring MVC

--- src/main/java/pu/fmi/wordle/logic/GameServiceImpl.java
+++ src/main/java/pu/fmi/wordle/logic/GameServiceImpl.java
@@ -2,10 +2,12 @@ package pu.fmi.wordle.logic;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.stereotype.Component;
 import pu.fmi.wordle.model.Game;
 import pu.fmi.wordle.model.GameRepo;
+import pu.fmi.wordle.model.Guess;
 import pu.fmi.wordle.model.WordRepo;
 
 @Component
@@ -27,6 +29,8 @@ public class GameServiceImpl implements GameService {
     game.setStartedOn(LocalDateTime.now());
     game.setWord(wordRepo.getRandom());
     game.setGuesses(new ArrayList<>(game.getMaxGuesses()));
+    updateAlphabetMatches(game);
+    gameRepo.save(game);
     return game;
   }
 
@@ -39,7 +43,56 @@ public class GameServiceImpl implements GameService {
 
   @Override
   public Game makeGuess(String id, String word) {
-    // TODO: implement
-    return null;
+    var game = getGame(id);
+    if (!wordRepo.exists(word)) throw new UnknownWordException(word);
+
+    var guess = new Guess();
+    guess.setWord(word);
+    guess.setMadeAt(LocalDateTime.now());
+    guess.setMatches(findMatches(game.getWord(), word));
+    game.getGuesses().add(guess);
+    updateAlphabetMatches(game);
+    return game;
+  }
+
+  private String findMatches(String chosen, String given) {
+    StringBuilder result = new StringBuilder();
+    for (int i = 0; i < given.length(); i++) {
+      char cc = given.charAt(i);
+      char mc = Guess.NO_MATCH;
+      if (chosen.charAt(i) == cc) {
+        mc = Guess.PLACE_MATCH;
+      } else if (chosen.indexOf(cc) >= 0) {
+        mc = Guess.LETTER_MATCH;
+      }
+
+      result.append(mc);
+    }
+
+    return result.toString();
+  }
+
+  private void updateAlphabetMatches(Game game) {
+    StringBuilder result = new StringBuilder();
+    game.getAlphabet()
+        .chars()
+        .map(letter -> getLetterMatch(game.getGuesses(), (char) letter))
+        .forEach(letter -> result.append((char) letter));
+    game.setAlphabetMatches(result.toString());
+  }
+
+  private char getLetterMatch(List<Guess> guesses, char letter) {
+    char match = 'U'; // Not used yet
+    for (var guess : guesses) {
+      var word = guess.getWord();
+      var matches = guess.getMatches();
+      for (int i = 0; i < word.length(); i++) {
+        if (word.charAt(i) == letter && match != Guess.PLACE_MATCH) {
+          match = matches.charAt(i);
+        }
+      }
+    }
+
+    return match;
   }
 }
